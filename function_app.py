f99f23e757cff5850a9848a58b328a530b7e6364
Diego Humpire <me@diegohumpire.me>
2024-04-24 21:25:41 -0500

    Session endpoint

--- function_app.py
+++ function_app.py
@@ -4,25 +4,3 @@ from main import app as fastapi_app
 
 app = func.AsgiFunctionApp(
     http_auth_level=func.AuthLevel.FUNCTION, app=fastapi_app)
-
-
-# @app.route(route="sessions")
-# def sessions(req: func.HttpRequest) -> func.HttpResponse:
-#     logging.info('Python HTTP trigger function processed a request.')
-
-#     name = req.params.get('name')
-#     if not name:
-#         try:
-#             req_body = req.get_json()
-#         except ValueError:
-#             pass
-#         else:
-#             name = req_body.get('name')
-
-#     if name:
-#         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-#     else:
-#         return func.HttpResponse(
-#             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-#             status_code=200
-#         )
--- main/__init__.py
+++ main/__init__.py
@@ -1,12 +1,44 @@
-import fastapi
+import os
+import uuid
 
-app = fastapi.FastAPI()
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel, EmailStr
+from redis import Redis
+
+app = FastAPI()
 
 
-@app.get("/sample")
-async def index():
+# Dependency
+def get_redis():
+    r = Redis(
+        host=os.environ['REDIS_HOST'],
+        port=42687,
+        password=os.environ['REDIS_PASSWORD'],
+        ssl=True
+    )
+
+    try:
+        yield r
+    finally:
+        r.close()
+
+
+class SessionInput(BaseModel):
+    email: EmailStr
+
+
+@app.post("/session")
+async def crate_session(input: SessionInput, redis: Redis = Depends(get_redis)):
+    uuid_ = str(uuid.uuid4())
+    redis.set(input.email, uuid_, ex=3600)
+
+    if redis.exists(input.email):
+        return {
+            "session": redis.get(input.email),
+        }
+
     return {
-        "info": "Try /hello/Shivani for parameterized route.",
+        "session": uuid_,
     }
 
 
